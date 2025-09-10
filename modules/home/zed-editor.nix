ab0890ba433b1ff04df50eddc11b9c487241aab3
b3col3 <antoine.goupil1998@gmail.com>
2025-09-10 19:44:11 +0200

    fix: Add zed-editor config for home manager

--- modules/home/zed-editor.nix
+++ modules/home/zed-editor.nix
@@ -1,20 +1,9 @@
-{ env-config, ... }:
-
 {
   programs = {
     zed-editor = {
       enable = true;
 
       extensions = ["nix" "toml" "Material Theme"];
-
-      theme = {
-        mode = "system";
-        light = "Material Theme Darker High Contrast";
-        dark = "Material Theme Darker High Contrast";
-      };
-      show_whitespaces = "all" ;
-      ui_font_size = 16;
-      buffer_font_size = 16;
 
       userSettings = {
         hour_format = "hour24";
@@ -23,21 +12,32 @@
           shell = "system";
           working_directory = "current_project_directory";
         };
-      };
 
-      lsp = {
-        rust-analyzer = {
-          binary = {
-            #  path = lib.getExe pkgs.rust-analyzer;
-            path_lookup = true;
-            };
+        theme = {
+          mode = "system";
+          light = "Material Theme Darker High Contrast";
+          dark = "Material Theme Darker High Contrast";
         };
-        nix = {
-          binary = {
-            path_lookup = true;
+        show_whitespaces = "all" ;
+        ui_font_size = 16;
+        buffer_font_size = 16;
+
+        lsp = {
+          rust-analyzer = {
+            binary = {
+              #  path = lib.getExe pkgs.rust-analyzer;
+              path_lookup = true;
+              };
+          };
+          nix = {
+            binary = {
+              path_lookup = true;
+            };
           };
         };
       };
+
+
     };
   };
 }
